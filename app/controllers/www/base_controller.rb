474ccae25a2de3bec2f281295cbc070bd1aa0c9a
unknown <jcunningham@.rpa.com>
2011-07-27 10:51:23 -0700

    Bugfix, missing port_string on redirect code.

--- app/controllers/www/base_controller.rb
+++ app/controllers/www/base_controller.rb
@@ -6,7 +6,7 @@ class Www::BaseController < ApplicationController
 
   def ensure_domain
   	unless request.domain(3).start_with?("www.")
-  		redirect_to "http://www.#{request.domain(2)}", :status => 301
+  		redirect_to "http://www.#{request.domain(2)+request.port_string}", :status => 301
   	end
   end
 

dfc7ac7fc7526bcd258aea2f5719be978ffad1e4
unknown <jcunningham@.rpa.com>
2011-07-29 12:51:18 -0700

    header and navigation pulled to application shared

---
+++ config/initializers/site_settings.rb
@@ -0,0 +1,29 @@
+
+ComGuygrundy::Application.configure do 
+	
+	SITE = {
+		:domain 									=> "guygrundy.com",
+		:author 									=> "Guy Grundy",
+		:author_short 						=> "Grundy",
+		:author_contact_email			=> "guy@guygrundy.com",
+		:title 										=> "",
+		
+		:twitter_name 						=> "guy_grundy", 											# no @ symbol
+		:twitter_id 							=> "",
+		
+		#:facebook_url							=> "",
+		:facebook_id							=> "833653760",
+		
+		:gravatar_id 							=> "",
+		
+		:google_ad_client 				=> "",
+		:google_ad_width 					=> 300,
+		:google_ad_height 				=> 300,
+		:google_ad_slot						=> "",
+		
+		:google_analytics_account => "",
+		
+		:disqus_short							=> "grundysblog"															#the shortname
+	}
+
+end
